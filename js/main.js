bab1877da8485c9c1ce98058a96f2df4a30f9297
Nycolas Santana <nycolassantana00@gmail.com>
2022-02-22 13:24:19 -0300

    add isotope, new section servicos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function ($) {
-
   window.onscroll = function () {
     if (window.pageYOffset > 140) {
       $("#header").addClass("active");
@@ -7,4 +6,17 @@ jQuery(document).ready(function ($) {
       $("#header").removeClass("active");
     }
   };
+
+  // ISOTOPE
+  let btns = $("#servicos .button-group button");
+
+  btns.click(function (e) {
+    $("#servicos .button-group button").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $("#servicos .grid").isotope({
+      filter: selector,
+    });
+  });
 });
