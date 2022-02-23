f92dc0be805e8700488a5e6798e86a625c9bc56f
Nycolas Santana <nycolassantana00@gmail.com>
2022-02-23 12:02:03 -0300

    project done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function ($) {
+  //FIXED HEADER
   window.onscroll = function () {
     if (window.pageYOffset > 140) {
       $("#header").addClass("active");
@@ -7,7 +8,7 @@ jQuery(document).ready(function ($) {
     }
   };
 
-  // ISOTOPE
+  //ISOTOPE
   let btns = $("#servicos .button-group button");
 
   btns.click(function (e) {
@@ -18,5 +19,44 @@ jQuery(document).ready(function ($) {
     $("#servicos .grid").isotope({
       filter: selector,
     });
+  });
+
+  $(window).on("load", function () {
+    $("#servicos .grid").isotope({
+      filter: "*",
+    });
+  });
+
+  //MAGNIFY
+  $(".grid .popup-link").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+      tPrev: "Anterior",
+      tNext: "Próxima",
+      tCounter: "%curr% de %total%",
+    },
+  });
+
+  //OWL
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    dots: true,
+    lazyLoad: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
   });
 });
